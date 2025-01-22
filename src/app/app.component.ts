c021498f30db6559bf7aa47b56ba765a5c1cda13
Ingvar Martin <guess.ein@gmail.com>
2025-01-22 13:53:12 +0200

    fixed deprecated subscribe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, ViewEncapsulation } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
@@ -7,14 +7,15 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
-import { Observable } from 'rxjs';
-import { map, shareReplay } from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
+import { map, tap, shareReplay, catchError } from 'rxjs/operators';
 
 import { DataService } from './data/services/data.service';
 import { updateDataStore } from './data/store/data.store';
 import { updateSettingsStore } from './data/store/chart.store';
 import { defaultChartSettings } from './data/interfaces/chart.interface';
 import { Dialog, DIALOG_DATA, DialogModule, } from '@angular/cdk/dialog';
+import { ChartData } from './data/interfaces/data.interface';
 
 @Component({
   selector: 'app-root',
@@ -44,19 +45,23 @@ export class AppComponent {
 
   constructor() {
     updateSettingsStore(defaultChartSettings);
-    this.dataService.getChartData().subscribe(
-      (val) => updateDataStore(val),
-      (err) => {
-        this.dialog.open(ErrorComponent, {
-          data: {
-            h1: 'Connection Error',
-            text: 'Please check your connection to the server and try again.<br>' +
-                  'The following text contains the error message:',
-            message: err.message,
-          },
-        });
-        this.responseError = err.message
-      });
+    this.dataService.getChartData()
+    .subscribe({
+      next: ((val) => updateDataStore(val)),
+      error: ((err) => this.openDialog(err))
+    });
+  }
+
+  openDialog(err: Error) {
+    this.dialog.open(ErrorComponent, {
+      data: {
+        h1: 'Connection Error',
+        text: 'Please check your connection to the server and try again.<br>' +
+              'The following text contains the error message:',
+        message: err.message,
+      },
+    });
+    this.responseError = err.message
   }
 
   isHandset$: Observable<boolean> = this.breakpointObserver
